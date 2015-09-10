d11ac17c15a3f6e46ff1e5d5d2e19e189800b43c
Serg <bkuek@mail.ru>
2015-09-10 03:24:01 +0300

    DB fix: adds view for ranging banners and some procedure stuff

--- .design/db.sql
+++ .design/db.sql
@@ -11,11 +11,13 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
 -- Дамп структуры базы данных karazin
+DROP DATABASE IF EXISTS `karazin`;
 CREATE DATABASE IF NOT EXISTS `karazin` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `karazin`;
 
 
 -- Дамп структуры для таблица karazin.banner
+DROP TABLE IF EXISTS `banner`;
 CREATE TABLE IF NOT EXISTS `banner` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `hint` varchar(1023) DEFAULT NULL,
@@ -23,29 +25,24 @@ CREATE TABLE IF NOT EXISTS `banner` (
   `priority` float NOT NULL,
   `href` text,
   `active` tinyint(1) NOT NULL DEFAULT '0',
+  `text` text,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Дамп данных таблицы karazin.banner: ~2 rows (приблизительно)
-DELETE FROM `banner`;
-/*!40000 ALTER TABLE `banner` DISABLE KEYS */;
-INSERT INTO `banner` (`id`, `hint`, `filename`, `priority`, `href`, `active`) VALUES
-	(1, 'hint1', '1.jpg', 25, 'google.com', 1),
-	(2, 'hint2', '2.jpg', 25, 'gmail.com', 1),
-	(3, 'hint3', '3.jpg', 50, 'yandex,ru', 1);
-/*!40000 ALTER TABLE `banner` ENABLE KEYS */;
+-- Экспортируемые данные не выделены.
 
 
 -- Дамп структуры для процедура karazin.getNewBannerId
+DROP PROCEDURE IF EXISTS `getNewBannerId`;
 DELIMITER //
 CREATE DEFINER=`root`@`localhost` PROCEDURE `getNewBannerId`()
     READS SQL DATA
     SQL SECURITY INVOKER
     COMMENT 'Returns new random banner ID'
 BEGIN
-SELECT SUM(`banner`.`priority`) INTO @cumulative_priority FROM `banner`;
+SELECT MAX(`rangedbanners`.`cumulative_sum`) INTO @cumulative_priority FROM `rangedbanners`;
 SET @pointer = getRandInt(1,@cumulative_priority);
- 	SELECT *, @pointer AS 'seed'
+ 	SELECT *, @pointer AS 'seed', @cumulative_priority AS 'cumulative_priority'
  		FROM `rangedbanners`
  		WHERE `rangedbanners`.`active` IS TRUE
  		AND `rangedbanners`.`cumulative_sum`>=@pointer
@@ -56,19 +53,21 @@ DELIMITER ;
 
 
 -- Дамп структуры для функция karazin.getRandInt
+DROP FUNCTION IF EXISTS `getRandInt`;
 DELIMITER //
 CREATE DEFINER=`root`@`localhost` FUNCTION `getRandInt`(`min` INT, `max` INT) RETURNS int(11)
     NO SQL
     SQL SECURITY INVOKER
     COMMENT 'Returns random integer from I to N'
 BEGIN
-SELECT FLOOR(min+RAND()*(max-min)) INTO @RESULT;
+SELECT FLOOR(RAND()*(max-min+1))+min INTO @RESULT;
 RETURN @RESULT;
 END//
 DELIMITER ;
 
 
 -- Дамп структуры для представление karazin.rangedbanners
+DROP VIEW IF EXISTS `rangedbanners`;
 -- Создание временной таблицы для обработки ошибок зависимостей представлений
 CREATE TABLE `rangedbanners` (
 	`BID` INT(11) NOT NULL,
@@ -78,11 +77,13 @@ CREATE TABLE `rangedbanners` (
 	`priority` FLOAT NOT NULL,
 	`href` TEXT NULL COLLATE 'utf8_general_ci',
 	`active` TINYINT(1) NOT NULL,
+	`text` TEXT NULL COLLATE 'utf8_general_ci',
 	`cumulative_sum` DOUBLE NULL
 ) ENGINE=MyISAM;
 
 
 -- Дамп структуры для таблица karazin.user
+DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
@@ -92,17 +93,13 @@ CREATE TABLE IF NOT EXISTS `user` (
   `password` varchar(32) NOT NULL,
   `level` decimal(10,0) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Дамп данных таблицы karazin.user: ~0 rows (приблизительно)
-DELETE FROM `user`;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` (`id`, `name`, `surname`, `email`, `isActive`, `password`, `level`) VALUES
-	(1, 'Sergiy', 'Lilikovych', 'sergiy.lilikovych@gmail.com', 1, '6d8e5be200a835beb77d899f00b890a5', 999999);
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+-- Экспортируемые данные не выделены.
 
 
 -- Дамп структуры для представление karazin.rangedbanners
+DROP VIEW IF EXISTS `rangedbanners`;
 -- Удаление временной таблицы и создание окончательной структуры представления
 DROP TABLE IF EXISTS `rangedbanners`;
 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` VIEW `rangedbanners` AS SELECT `banner`.id as 'BID', `banner`.*,
@@ -111,6 +108,7 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` VIEW `rangedbanners` AS SE
            WHERE `banner`.id <= BID
 			  ORDER BY `banner`.`priority` ASC) AS cumulative_sum
     FROM `banner`
+    WHERE `banner`.`active` IS TRUE
 ORDER BY `banner`.`priority` ASC ;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
